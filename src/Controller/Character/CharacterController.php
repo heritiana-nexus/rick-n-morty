e837bef3a8190bf77789e254281179a839127c60
Heritiana <hrakotomalala@bocasay.com>
2021-07-28 16:28:53 +0300

    test get characters from database

--- src/Controller/Character/CharacterController.php
+++ src/Controller/Character/CharacterController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller\Character;
 
-
+use App\Manager\Character\CharacterManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CharacterController extends AbstractController
 {
     /**
+     * @var CharacterManager
+     */
+    private $manager;
+
+    /**
      * CharacterController constructor.
      */
-    public function __construct()
+    public function __construct(CharacterManager $characterManager)
     {
+        $this->manager = $characterManager;
     }
 
+    /**
+     * @Route(
+     *     name="get_character_list",
+     *     path="/api/character",
+     *     methods={"GET"}
+     * )
+     */
+    public function getCharactersAction(Request $request)
+    {
+        $response = $this->manager->getCharacters();
+        dd('char heloo');
+    }
 
 }
--- src/Manager/Character/CharacterManager.php
+++ src/Manager/Character/CharacterManager.php
@@ -2,12 +2,26 @@
 
 namespace App\Manager\Character;
 
+use App\Repository\CharacterRepository;
+
 class CharacterManager
 {
     /**
+     * @var CharacterRepository
+     */
+    private $repository;
+
+    /**
      * CharacterManager constructor.
      */
-    public function __construct()
+    public function __construct(CharacterRepository $repository)
     {
+        $this->repository = $repository;
+    }
+
+    public function getCharacters()
+    {
+        $characters = $this->repository->findAll();
+
     }
 }
